fe6fcc524fadd4148e077f2eac1a4016d329a666
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-21 02:36:30 +0700

    Global and Scoped CSS was discussed

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="backdrop">
         <div class="modal">
+            <h1>Modal title</h1>
             <p>Modal Content</p>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>/* scoped WILL LIMIT THE STYLESHEET AND ONLY AFFECT THIS COMPONENT (Modal.vue) */
 .modal {
     width: 400px;
     padding: 20px;
@@ -21,5 +22,10 @@
     background: rgba(0, 0, 0, 0.5);
     width: 100%;
     height: 100%;
+}
+
+h1 {
+    color: red;
+    border: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./assets/global.css" // THIS IS HOW YOU CALL CSS FILE
 
 createApp(App).mount('#app')
